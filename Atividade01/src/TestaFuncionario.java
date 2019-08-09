617012f1528259869f7a3f55cdddecc16721828c
18214290010 <18214290010@OLAB04-D22.academico.iesb.br>
2019-08-08 22:21:31 -0300

    Novo versionamento

---
+++ Atividade01/src/TestaFuncionario.java
@@ -0,0 +1,12 @@
+
+public class TestaFuncionario {
+
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario(12345, "João", Funcionario.SEXO_MASCULINO, 1000);
+        System.out.println(funcionario.getMatricula() + " - "
+        + funcionario.getNome() + " - "
+        + funcionario.getSexo() + " - "         
+        + funcionario.getSalario());
+                 }
+
+}
